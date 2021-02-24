abbeb4e888fea2e5c74f4b9082d24e51fe81e25d
Jack O'Hara <jack.ohara@sorted.com>
2021-02-24 21:49:09 +0000

    Added a get request to the uk gov covid api

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,15 @@
 <script>
-	let name = 'world';
+  const fetchData = (async () => {
+    const response = await fetch('https://api.coronavirus.data.gov.uk/v1/data?filters=areaType=nation;areaName=england&structure={"date":"date","newCases":"newCasesByPublishDate", "cumCases":"cumCasesByPublishDate"}');
+    console.log(response);
+    return await response.json();
+  })();
 </script>
 
-<h1>Hello {name}!</h1>
+{#await fetchData}
+  <p>Loading...</p>
+{:then data}
+  <p>{JSON.stringify(data)}</p>
+{:catch error}
+  <p>Oops... {error}</p>
+{/await}
