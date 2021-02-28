d6acf18d8c3ee28f716fffb1c9a8e42331c1ddef
Jack O'Hara <jack.ohara@sorted.com>
2021-02-28 10:30:16 +0000

    Added typescript support

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,32 @@
-<script>
+<script lang="ts">
+  const AreaType: string = "overview";
+  const AreaName: string = "england";
+
+  const filters = [
+        `areaType=${ AreaType }`,
+        //`areaName=${ AreaName }`
+    ];
+  const structure = {
+      date: "date",
+      //name: "areaName",
+      code: "areaCode",
+      cases: {
+          daily: "newCasesByPublishDate",
+          cumulative: "cumCasesByPublishDate"
+      },
+      deaths: {
+          daily: "newDeathsByDeathDate",
+          cumulative: "cumDeathsByDeathDate"
+      }
+  };
+
+  const apiParams: string = `filters=${ filters.join(";") }&structure=${ JSON.stringify(structure) }`;
+  const encodedParams: string = encodeURI(apiParams);
+
   const fetchData = (async () => {
-    const response = await fetch('https://api.coronavirus.data.gov.uk/v1/data?filters=areaType=nation;areaName=england&structure={"date":"date","newCases":"newCasesByPublishDate", "cumCases":"cumCasesByPublishDate"}');
+    const url: string = `https://api.coronavirus.data.gov.uk/v1/data?${encodedParams}`;
+    console.log(url);
+    const response = await fetch(url);
     console.log(response);
     return await response.json();
   })();
